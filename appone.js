6439452c07239b8f8f0ec686d1c2d34858709252
poojaarora2912 <poojaarora2912@gmail.com>
2022-04-21 15:43:58 +0530

    add: star trek project
    add: star trek project

--- appone.js
+++ appone.js
@@ -1,8 +1,12 @@
-var translatebutton = document.querySelector("#translate");
+var translateklingon = document.querySelector("#translateklingon");
+var translatevulcan = document.querySelector("#translatevulcan");
+var translateromulan = document.querySelector("#translateromulan");
 var inputtext = document.querySelector("#input");
 var outputtext = document.querySelector("#output");
 
-var serverURL = "https://api.funtranslations.com/translate/braille.json"
+var serverURLKlingon = "https://api.funtranslations.com/translate/klingon.jsonn";
+var serverURLVulcan = "https://api.funtranslations.com/translate/vulcan.json";
+var serverURLRomulan = "https://api.funtranslations.com/translate/romulan.json";
 
 function errorHandler(error){
     console.log("Error occured", error);
@@ -10,20 +14,58 @@ function errorHandler(error){
 }
 
 
-function clickHandler(){
+function clickHandlerKlingon(){
     var inputtext1 = inputtext.value;
+  
+    
+        fetch(getTranslationKlingon(inputtext1))
+        .then(response =>response.json())
+        .then(json =>{
+            var translatedtext = json.contents.translated;
+             outputtext.innerText = translatedtext;
+        })
+        .catch(errorHandler)
+    
+    };
+function clickHandlerVulcan(){
+        var inputtext1 = inputtext.value;
+      
+        
+            fetch(getTranslationVulcan(inputtext1))
+            .then(response =>response.json())
+            .then(json =>{
+                var translatedtext = json.contents.translated;
+                 outputtext.innerText = translatedtext;
+            })
+            .catch(errorHandler)
+        
+        };
+function clickHandlerRomulan(){
+        var inputtext1 = inputtext.value;
+          
+            
+            fetch(getTranslationRomulan(inputtext1))
+            .then(response =>response.json())
+            .then(json =>{
+                 var translatedtext = json.contents.translated;
+                 outputtext.innerText = translatedtext;
+            })
+                .catch(errorHandler)
+            
+            };
 
-    fetch(getTranslation(inputtext1))
-    .then(response =>response.json())
-    .then(json =>{
-        var translatedtext = json.contents.translated;
-         outputtext.innerText = translatedtext;
-    })
-    .catch(errorHandler)
-};
-
-function getTranslation(text){
-    return serverURL+"?"+"text="+text;
+function getTranslationKlingon(text){
+    return serverURLKlingon+"?"+"text="+text;
+}
+function getTranslationVulcan(text){
+    return serverURLVulcan+"?"+"text="+text;
+}
+function getTranslationRomulan(text){
+    return serverURLRomulan+"?"+"text="+text;
 }
 
-translatebutton.addEventListener("click", clickHandler)
+
+
+translateklingon.addEventListener("click", clickHandlerKlingon())
+translatevulcan.addEventListener("click", clickHandlerVulcan)
+translateromulan.addEventListener("click", clickHandlerRomulan)
